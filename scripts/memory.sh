49de038ebcb8b9a8bbdc2b4ef84d4458b9e52cd6
anguzz <santoyox714@gmail.com>
2025-09-23 22:01:40 -0700

    Update storage and memory modules with df-style percent + progress bars

--- scripts/memory.sh
+++ scripts/memory.sh
@@ -1,11 +1,35 @@
 #!/usr/bin/env bash
-# memory.sh - shows memory usage
 
 CRT_GREEN="\e[92m"
 RESET="\e[0m"
 
+#  values from free -h (human readable)
 mem_total=$(free -h | awk '/Mem:/ {print $2}')
 mem_used=$(free -h | awk '/Mem:/ {print $3}')
 mem_free=$(free -h | awk '/Mem:/ {print $4}')
 
-echo -e "${CRT_GREEN}Memory:${RESET} $mem_used / $mem_total (Free: $mem_free)"
+# gets percentage used (integer, like "free" reports)
+mem_perc=$(free | awk '/Mem:/ {printf "%d", ($3/$2)*100}')
+
+echo -e "${CRT_GREEN}Memory:${RESET} $mem_used / $mem_total (Free: $mem_free) (${mem_perc}%)"
+
+# --- progress bar ---
+draw_bar() {
+  local percent=$1
+  local width=${2:-30}
+
+  local fill=$(( percent * width / 100 ))
+  local empty=$(( width - fill ))
+
+  printf "["
+  for ((i=0; i<fill; i++)); do
+    printf "%b█%b" "$CRT_GREEN" "$RESET"
+  done
+  for ((i=0; i<empty; i++)); do
+    printf " "
+  done
+  printf "] %s%%\n" "$percent"
+}
+
+draw_bar "$mem_perc" 30
+
